Xilinx/gst-plugins-good

commit a7c801dde867f56292fb78265a3a798ab63e393d

v4l2object: XLNX-LL: add XLNX-LL caps feature
Advertise XLNX-LL in caps on capture devices if syncIP dev file is present.

Upstream-Status: Invalid [Xilinx specific]
Signed-off-by: Guillaume Desmottes <guillaume.desmottes@collabora.com>

sys/v4l2/gstv4l2object.c
@@ -1587,6 +1587,13 @@ done:
   return template;
 }
 
+#define SYNC_IP_DEV_ENCODER "/dev/xvsfsync0"
+
+static gboolean
+xlnx_ll_supported (void)
+{
+  return g_file_test (SYNC_IP_DEV_ENCODER, G_FILE_TEST_EXISTS);
+}
 
 static GstCaps *
 gst_v4l2_object_get_caps_helper (GstV4L2FormatFlags flags)
@@ -1640,6 +1647,13 @@ gst_v4l2_object_get_caps_helper (GstV4L2FormatFlags flags)
         gst_caps_append_structure (caps, alt_s);
         gst_caps_set_features (caps, gst_caps_get_size (caps) - 1,
             gst_caps_features_new (GST_CAPS_FEATURE_FORMAT_INTERLACED, NULL));
+
+        /* Add one variant for XLNX LL */
+        alt_s = gst_structure_copy (structure);
+
+        gst_caps_append_structure (caps, alt_s);
+        gst_caps_set_features (caps, gst_caps_get_size (caps) - 1,
+            gst_caps_features_new (GST_CAPS_FEATURE_MEMORY_XLNX_LL, NULL));
       }
     }
   }
@@ -2601,6 +2615,16 @@ done:
 }
 
 static void
+dup_struct_with_xlnx_ll (GstCaps * caps, GstStructure * s)
+{
+  GstStructure *copy;
+
+  copy = gst_structure_copy (s);
+  gst_caps_append_structure_full (caps, copy,
+      gst_caps_features_new (GST_CAPS_FEATURE_MEMORY_XLNX_LL, NULL));
+}
+
+static void
 gst_v4l2_object_update_and_append (GstV4l2Object * v4l2object,
     guint32 format, GstCaps * caps, GstStructure * s)
 {
@@ -2642,6 +2666,15 @@ gst_v4l2_object_update_and_append (GstV4l2Object * v4l2object,
 
   if (alt_s) {
     check_alternate_and_append_struct (caps, alt_s);
+  }
+
+  if ((v4l2object->type == V4L2_BUF_TYPE_VIDEO_CAPTURE
+          || v4l2object->type == V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE)
+      && xlnx_ll_supported ()) {
+    dup_struct_with_xlnx_ll (caps, s);
+    if (alt_s) {
+      dup_struct_with_xlnx_ll (caps, alt_s);
+    }
   }
 }
 

commit 4765614a323ae661ff534faf09dedd10ca6c5f27

v4l2src: XLNX-LL: only set caps template feature on v4l2src
Only v4l2src supports XLNX-LL so only include it in its caps template.

Fix a regression introduced by a7c801dde867f56292fb78265a3a798ab63e393d
preventing v4l2video4convert to be registered as gst_v4l2_is_transform()
is checking if both caps are a subset of gst_v4l2_object_get_raw_caps().

Upstream-Status: Invalid [Xilinx specific]
Signed-off-by: Guillaume Desmottes <guillaume.desmottes@collabora.com>

sys/v4l2/gstv4l2object.c
@@ -1646,13 +1646,6 @@ gst_v4l2_object_get_caps_helper (GstV4L2FormatFlags flags)
         gst_caps_append_structure (caps, alt_s);
         gst_caps_set_features (caps, gst_caps_get_size (caps) - 1,
             gst_caps_features_new (GST_CAPS_FEATURE_FORMAT_INTERLACED, NULL));
-
-        /* Add one variant for XLNX LL */
-        alt_s = gst_structure_copy (structure);
-
-        gst_caps_append_structure (caps, alt_s);
-        gst_caps_set_features (caps, gst_caps_get_size (caps) - 1,
-            gst_caps_features_new (GST_CAPS_FEATURE_MEMORY_XLNX_LL, NULL));
       }
     }
   }

sys/v4l2/gstv4l2src.c
@@ -138,6 +138,8 @@ gst_v4l2src_class_init (GstV4l2SrcClass * klass)
   GstElementClass *element_class;
   GstBaseSrcClass *basesrc_class;
   GstPushSrcClass *pushsrc_class;
+  GstCaps *caps, *caps_xlnx_ll;
+  guint i;
 
   gobject_class = G_OBJECT_CLASS (klass);
   element_class = GST_ELEMENT_CLASS (klass);
@@ -176,10 +178,35 @@ gst_v4l2src_class_init (GstV4l2SrcClass * klass)
       "Edgard Lima <edgard.lima@gmail.com>, "
       "Stefan Kost <ensonic@users.sf.net>");
 
+  caps = gst_v4l2_object_get_all_caps ();
+  /* We don't own caps */
+  caps = gst_caps_copy (caps);
+  caps_xlnx_ll = gst_caps_new_empty ();
+
+  for (i = 0; i < gst_caps_get_size (caps); i++) {
+    GstStructure *s = gst_caps_get_structure (caps, i);
+
+    if (gst_structure_has_name (s, "video/x-raw")) {
+      GstCapsFeatures *features;
+
+      features = gst_caps_get_features (caps, i);
+      features = gst_caps_features_copy (features);
+      gst_caps_features_remove (features,
+          GST_CAPS_FEATURE_MEMORY_SYSTEM_MEMORY);
+      gst_caps_features_add (features, GST_CAPS_FEATURE_MEMORY_XLNX_LL);
+
+      gst_caps_append_structure_full (caps_xlnx_ll, gst_structure_copy (s),
+          features);
+    }
+  }
+
+  caps = gst_caps_merge (caps, caps_xlnx_ll);
+
   gst_element_class_add_pad_template
       (element_class,
-      gst_pad_template_new ("src", GST_PAD_SRC, GST_PAD_ALWAYS,
-          gst_v4l2_object_get_all_caps ()));
+      gst_pad_template_new ("src", GST_PAD_SRC, GST_PAD_ALWAYS, caps));
+
+  gst_caps_unref (caps);
 
   basesrc_class->get_caps = GST_DEBUG_FUNCPTR (gst_v4l2src_get_caps);
   basesrc_class->start = GST_DEBUG_FUNCPTR (gst_v4l2src_start);