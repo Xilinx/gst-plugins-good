Xilinx/gst-plugins-good

commit 0496ec8445cc930ad0e828231b4098216f1ad6d4

v4l2: Add support for ARIB STD B67 (HLG) EOTF
There is no support for ARIB STD B67 (HLG) EOTF in v4l2 and gstreamer.
This patch adds the enum from linux-xlnx and also adds support to
convert to gstreamer enums. This is currently set to Xilinx Specific as
v4l2 EOTF patches are still pending to be upstreamed.

Upstream-Status: Invalid [Xilinx Specific]
Signed-off-by: Dylan Yip <dylan.yip@xilinx.com>
Signed-off-by: Jeegar Patel <jeegar.patel@xilinx.com>

Acked-for-series:Varunkumar Allagadapa <varunkumar.allagadapa@xilinx.com>

sys/v4l2/gstv4l2object.c
@@ -2269,6 +2269,9 @@ gst_v4l2_object_get_colorspace (GstV4l2Object * v4l2object,
     case V4L2_XFER_FUNC_SMPTE2084:
       cinfo->transfer = GST_VIDEO_TRANSFER_SMPTE2084;
       break;
+    case V4L2_XFER_FUNC_HLG:
+      cinfo->transfer = GST_VIDEO_TRANSFER_ARIB_STD_B67;
+      break;
     case V4L2_XFER_FUNC_DEFAULT:
       /* nothing, just use defaults for colorspace */
       break;
@@ -3668,6 +3671,9 @@ gst_v4l2_object_set_format_full (GstV4l2Object * v4l2object, GstCaps * caps,
       break;
     case GST_VIDEO_COLOR_MATRIX_UNKNOWN:
       /* We let the driver pick a default one */
+      break;
+    case GST_VIDEO_TRANSFER_ARIB_STD_B67:
+      transfer = V4L2_XFER_FUNC_HLG;
       break;
     default:
       GST_WARNING_OBJECT (v4l2object->dbg_obj,

sys/v4l2/ext/videodev2.h
@@ -288,6 +288,7 @@ enum v4l2_xfer_func {
 	V4L2_XFER_FUNC_NONE        = 5,
 	V4L2_XFER_FUNC_DCI_P3      = 6,
 	V4L2_XFER_FUNC_SMPTE2084   = 7,
+	V4L2_XFER_FUNC_HLG         = 8,
 };
 
 /*

commit adc0e0329d96be5a2cd7010f5b1eaccabd12ff16

v4l2: Fix transfer function for HLG case
This patch correctly sets the transer function and fixes the below error
introduced by commit
0496ec844 v4l2: Add support for ARIB STD B67 (HLG) EOTF.

Pipeline is PREROLLED ...
Setting pipeline to PLAYING ...
0:00:00.111058237   916 0xaaab1cab5180 WARN                    v4l2 gstv4l2object.c:3764:gst_v4l2_object_set_format_full:<v4l2src0:src> Unknown colorimetry transfer 15
0:00:00.111171698   916 0xaaab1cab5180 WARN                    v4l2 gstv4l2object.c:3764:gst_v4l2_object_set_format_full:<v4l2src0:src> Unknown colorimetry transfer 15
0:00:00.111197900   916 0xaaab1c9be100 WARN                 v4l2src gstv4l2src.c:794:gst_v4l2src_query:<v4l2src0> Can't give latency since framerate isn't fixated !
0:00:00.111228774   916 0xaaab1cab5180 WARN                 default v4l2-utils.c:189:gst_v4l2_error:<v4l2src0> error: Device '/dev/video0' does not support interleaved interlacing
0:00:00.111262507   916 0xaaab1cab5180 WARN                 default v4l2-utils.c:192:gst_v4l2_error:<v4l2src0> error: Device wants progressive interlacing
ERROR: from element /GstPipeline:pipeline0/GstV4l2Src:v4l2src0: Device '/dev/video0' does not support interleaved interlacing
Additional debug info:
../git/sys/v4l2/gstv4l2object.c(4208): gst_v4l2_object_set_format_full (): /GstPipeline:pipeline0/GstV4l2Src:v4l2src0:
Device wants progressive interlacing
ERROR: pipeline doesn't want to preroll.
0:00:00.111522163   916 0xaaab1cab5180 WARN                 basesrc gstbasesrc.c:3127:gst_base_src_loop:<v4l2src0> error: Internal data stream error.
Execution ended after 0:00:00.000486738
0:00:00.111556116   916 0xaaab1cab5180 WARN                 basesrc gstbasesrc.c:3127:gst_base_src_loop:<v4l2src0> error: streaming stopped, reason not-negotiated (-4)
Setting pipeline to NULL ...
ERROR: from element /GstPipeline:pipeline0/GstV4l2Src:v4l2src0: Internal data stream error.
Additional debug info:

sys/v4l2/gstv4l2object.c
@@ -3715,9 +3715,6 @@ gst_v4l2_object_set_format_full (GstV4l2Object * v4l2object, GstCaps * caps,
     case GST_VIDEO_COLOR_MATRIX_UNKNOWN:
       /* We let the driver pick a default one */
       break;
-    case GST_VIDEO_TRANSFER_ARIB_STD_B67:
-      transfer = V4L2_XFER_FUNC_HLG;
-      break;
     default:
       GST_WARNING_OBJECT (v4l2object->dbg_obj,
           "Unknown colorimetry matrix %d", info.colorimetry.matrix);
@@ -3756,6 +3753,9 @@ gst_v4l2_object_set_format_full (GstV4l2Object * v4l2object, GstCaps * caps,
       GST_WARNING_OBJECT (v4l2object->dbg_obj,
           "LOG 100, 316 transfer functions not supported");
       /* FIXME No known sensible default, maybe AdobeRGB ? */
+      break;
+    case GST_VIDEO_TRANSFER_ARIB_STD_B67:
+      transfer = V4L2_XFER_FUNC_HLG;
       break;
     case GST_VIDEO_TRANSFER_UNKNOWN:
       /* We let the driver pick a default one */